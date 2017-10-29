0e06d9d6dd7c1fd5a9a874796b73d6a30afcef16
u1 <u1.kobayashi@gmail.com>
2017-10-30 00:05:34 +0900

    Convert customview package to Kotlin.

---
+++ app/src/main/java/com/u1fukui/bbs/customview/ArrayRecyclerAdapter.kt
@@ -0,0 +1,45 @@
+package com.u1fukui.bbs.customview
+
+import android.support.annotation.UiThread
+import android.support.v7.widget.RecyclerView
+
+abstract class ArrayRecyclerAdapter<T, VH : RecyclerView.ViewHolder> @JvmOverloads constructor(protected val list: MutableList<T> = ArrayList()) : RecyclerView.Adapter<VH>() {
+
+    @UiThread
+    fun reset(items: Collection<T>) {
+        clear()
+        addAll(items)
+        notifyDataSetChanged()
+    }
+
+    fun clear() {
+        list.clear()
+    }
+
+    fun addAll(items: Collection<T>) {
+        list.addAll(items)
+    }
+
+    fun getItem(position: Int): T {
+        return list[position]
+    }
+
+    fun addItem(item: T) {
+        list.add(item)
+    }
+
+    fun addAll(position: Int, items: Collection<T>) {
+        list.addAll(position, items)
+    }
+
+    @UiThread
+    fun addAllWithNotify(items: Collection<T>) {
+        val position = itemCount
+        addAll(items)
+        notifyItemInserted(position)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+}
---
+++ app/src/main/java/com/u1fukui/bbs/customview/RecyclerViewScrolledEndSubject.kt
@@ -0,0 +1,51 @@
+package com.u1fukui.bbs.customview
+
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+
+import io.reactivex.subjects.PublishSubject
+import io.reactivex.subjects.Subject
+
+class RecyclerViewScrolledEndSubject(private var recyclerView: RecyclerView?) {
+
+    private val subject = PublishSubject.create<Any>()
+
+    private val listener = object : RecyclerView.OnScrollListener() {
+        override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
+            val manager = recyclerView!!.layoutManager as LinearLayoutManager
+
+            val visibleCount = recyclerView.childCount
+            val totalCount = manager.itemCount
+            val firstPos = manager.findFirstVisibleItemPosition()
+
+            if (totalCount <= firstPos + visibleCount + THRESHOLD_COUNT) {
+                subject.onNext(Irrelevant.INSTANCE)
+            }
+        }
+    }
+
+    internal enum class Irrelevant {
+        INSTANCE
+    }
+
+    init {
+        if (recyclerView.getLayoutManager() !is LinearLayoutManager) {
+            throw IllegalArgumentException("RecyclerView needs to have LinearLayoutManager")
+        }
+        this.recyclerView!!.addOnScrollListener(listener)
+    }
+
+    fun connect(): Subject<*> {
+        return subject
+    }
+
+    fun shutdown() {
+        recyclerView!!.removeOnScrollListener(listener)
+        recyclerView = null
+    }
+
+    companion object {
+
+        private val THRESHOLD_COUNT = 5
+    }
+}
