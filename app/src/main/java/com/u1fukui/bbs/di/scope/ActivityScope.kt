97013bc7d5769caf6bbc682a38150114e59b0360
u1 <u1.kobayashi@gmail.com>
2017-10-29 23:58:01 +0900

    Replace to kotlin annotation.

--- app/src/main/java/com/u1fukui/bbs/di/scope/ActivityScope.kt
+++ app/src/main/java/com/u1fukui/bbs/di/scope/ActivityScope.kt
@@ -1,13 +1,8 @@
 package com.u1fukui.bbs.di.scope
 
-import java.lang.annotation.Documented
-import java.lang.annotation.Retention
-
 import javax.inject.Scope
 
-import java.lang.annotation.RetentionPolicy.RUNTIME
-
 @Scope
-@Documented
-@Retention(RUNTIME)
+@MustBeDocumented
+@Retention(AnnotationRetention.RUNTIME)
 annotation class ActivityScope
