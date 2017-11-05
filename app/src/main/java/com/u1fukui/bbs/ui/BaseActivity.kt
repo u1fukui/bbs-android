073a9723fbb7b9cecd57fd757162803296ca72a0
u1 <u1.kobayashi@gmail.com>
2017-11-05 22:58:54 +0900

    Refactor BaseActivity.

--- app/src/main/java/com/u1fukui/bbs/ui/BaseActivity.kt
+++ app/src/main/java/com/u1fukui/bbs/ui/BaseActivity.kt
@@ -9,21 +9,23 @@ open class BaseActivity : DaggerAppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            android.R.id.home -> onBackPressed()
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
         }
         return super.onOptionsItemSelected(item)
     }
 
     protected fun initToolbar(toolbar: Toolbar, canBack: Boolean) {
         setSupportActionBar(toolbar)
-        val bar = supportActionBar
-        if (bar != null) {
-            bar.title = toolbar.title
+        supportActionBar?.apply {
+            title = toolbar.title
             if (canBack) {
-                bar.setDisplayHomeAsUpEnabled(true)
-                bar.setDisplayShowHomeEnabled(true)
-                bar.setDisplayShowTitleEnabled(true)
-                bar.setHomeButtonEnabled(true)
+                setDisplayHomeAsUpEnabled(true)
+                setDisplayShowHomeEnabled(true)
+                setDisplayShowTitleEnabled(true)
+                setHomeButtonEnabled(true)
             }
         }
     }
