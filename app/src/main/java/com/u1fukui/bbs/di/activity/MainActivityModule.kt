d2d933761626057e0e556972417d4808082c19bc
u1 <u1.kobayashi@gmail.com>
2017-12-17 23:58:52 +0900

    Add modules for favorite / history list fragments.

--- app/src/main/java/com/u1fukui/bbs/di/activity/MainActivityModule.kt
+++ app/src/main/java/com/u1fukui/bbs/di/activity/MainActivityModule.kt
@@ -1,10 +1,14 @@
 package com.u1fukui.bbs.di.activity
 
 import com.u1fukui.bbs.di.fragment.CategoryThreadListFragmentModule
+import com.u1fukui.bbs.di.fragment.FavoriteThreadListFragmentModule
+import com.u1fukui.bbs.di.fragment.HistoryThreadListFragmentModule
 import com.u1fukui.bbs.di.fragment.HomeFragmentModule
 import com.u1fukui.bbs.di.scope.FragmentScope
 import com.u1fukui.bbs.ui.main.home.CategoryThreadListFragment
 import com.u1fukui.bbs.ui.main.home.HomeFragment
+import com.u1fukui.bbs.ui.main.mypage.FavoriteThreadListFragment
+import com.u1fukui.bbs.ui.main.mypage.HistoryThreadListFragment
 
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
@@ -22,5 +26,13 @@ class MainActivityModule {
         @FragmentScope
         @ContributesAndroidInjector(modules = arrayOf(CategoryThreadListFragmentModule::class))
         internal abstract fun categoryThreadListFragemnt(): CategoryThreadListFragment
+
+        @FragmentScope
+        @ContributesAndroidInjector(modules = arrayOf(FavoriteThreadListFragmentModule::class))
+        internal abstract fun favoriteThreadListFragment(): FavoriteThreadListFragment
+
+        @FragmentScope
+        @ContributesAndroidInjector(modules = arrayOf(HistoryThreadListFragmentModule::class))
+        internal abstract fun historyThreadListFragment(): HistoryThreadListFragment
     }
 }
