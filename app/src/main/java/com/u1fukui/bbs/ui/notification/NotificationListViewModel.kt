0aa8c65c48690185c240d14f10427a7dffb06973
u1 <u1.kobayashi@gmail.com>
2018-03-25 13:33:07 +0900

    Apply AAC ViewModel to NotificationListBindingModel.

---
+++ app/src/main/java/com/u1fukui/bbs/ui/notification/NotificationListViewModel.kt
@@ -0,0 +1,79 @@
+package com.u1fukui.bbs.ui.notification
+
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProvider
+import android.databinding.ObservableArrayList
+import android.databinding.ObservableList
+import com.u1fukui.bbs.customview.ErrorView
+import com.u1fukui.bbs.helper.LoadingManager
+import com.u1fukui.bbs.repository.NotificationListRepository
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
+
+class NotificationListViewModel(
+        private val repository: NotificationListRepository
+) : ViewModel(), ErrorView.ErrorViewListener {
+
+    val loadingManager = LoadingManager()
+
+    //TOOD: 整理する
+    internal val notificationBindingModelList: ObservableList<NotificationBindingModel> = ObservableArrayList()
+
+    private val compositeDisposable: CompositeDisposable = CompositeDisposable()
+
+    fun start() {
+        if (notificationBindingModelList.isEmpty()) {
+            fetchNotificationList()
+        }
+    }
+
+    private fun fetchNotificationList() {
+        if (loadingManager.isLoading) {
+            return
+        }
+        loadingManager.startLoading()
+
+        repository.fetchNotificationList()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeBy(
+                        onSuccess = { response ->
+                            val bindingModelList = response.map { NotificationBindingModel(it) }
+                            renderNotificationList(bindingModelList)
+                        },
+                        onError = {
+                            loadingManager.showErrorView(it)
+                        }
+                )
+                .addTo(compositeDisposable)
+    }
+
+    private fun renderNotificationList(list: List<NotificationBindingModel>) {
+        notificationBindingModelList.clear()
+        notificationBindingModelList.addAll(list)
+
+        loadingManager.showContentView()
+    }
+
+    override fun onClickReloadButton() {
+        fetchNotificationList()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
+    }
+
+    class Factory(
+            private val repository: NotificationListRepository
+    ) : ViewModelProvider.NewInstanceFactory() {
+
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(modelClass: Class<T>): T = NotificationListViewModel(
+                repository
+        ) as T
+    }
+}
